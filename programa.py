16e75ad5e6ce3578935fceb4a1833c22c0a66a2c
Sergio <sergio@sergio-VirtualBox.(none)>
2014-06-02 21:17:29 +0200

    Añadida la multiplicacion.

--- programa.py
+++ programa.py
@@ -15,14 +15,21 @@ def resta(x,y):
 	sum= y-x
 	return sum
 
+def mult(x,y):
+	print "Haciendo multiplicion..."
+	mul=x*y
+	return mul
+
 def main():
 	numer= sys.argv[2]	
 	numero= sys.argv[1]
 	sumados= suma(int(numer),int(numero))
 	restados= resta(int(numer),int(numero))
+	multi= mult(int(numer),int(numero))
 	escribe('Has introducido '+numero+' y '+numer+' en la entrada')
 	escribe('Y la suma de ambos es '+str(sumados))
 	escribe('Y la resta del primero menos el segundo es '+str(restados))
+	escribe('La multiplicacion  de ambos es '+str(multi))
 
 if (len(sys.argv)<2):
 	print "Error: falta argumento"
