3643067d0e8af34a36327b8ae6e2c659f2a89fbd
Sergio <sergio@sergio-VirtualBox.(none)>
2014-06-02 21:16:00 +0200

    Segunda version con la resta de numeros.

--- programa.py
+++ programa.py
@@ -10,12 +10,19 @@ def suma(x,y):
 	sum= x+y
 	return sum
 
+def resta(x,y):
+	print "Haciendo resta..."
+	sum= y-x
+	return sum
+
 def main():
 	numer= sys.argv[2]	
 	numero= sys.argv[1]
 	sumados= suma(int(numer),int(numero))
+	restados= resta(int(numer),int(numero))
 	escribe('Has introducido '+numero+' y '+numer+' en la entrada')
 	escribe('Y la suma de ambos es '+str(sumados))
+	escribe('Y la resta del primero menos el segundo es '+str(restados))
 
 if (len(sys.argv)<2):
 	print "Error: falta argumento"
